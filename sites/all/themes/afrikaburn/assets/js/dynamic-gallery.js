8206c344bd3d322daeece6c13083b9811d2bccad
Andrew Paterson <andrew@hyraxbio.co.za>
2018-06-14 13:16:22 +0200

    Headers flow along with the images correctly.

--- sites/all/themes/afrikaburn/assets/js/dynamic-gallery.js
+++ sites/all/themes/afrikaburn/assets/js/dynamic-gallery.js
@@ -12,36 +12,55 @@ jQuery(document).ready(function() {
   var mainContentText = jQuery('noscript').text();
   var mainContentHTML = jQuery.parseHTML(mainContentText);
   var pager = jQuery(mainContentHTML).find('ul.pager');
-  var table = jQuery(mainContentHTML).find('table');
+  var tables = jQuery(mainContentHTML).find('table');
   var header = jQuery(mainContentHTML).find('h3').text();
+
 
   // Add the gallery element to the page.
   var galleryElement =
-  `<h3>${header}</h3>
+  `
   <div class="image-gallery" data-thumbnail-dimensions="5:4"></div>
   <div class="clr"></div>
   <div class="load-more-images"><div class="loader"></div></div>`
   jQuery('#main-content-strip .body-content').append(galleryElement);
   jQuery('#main-content-strip .body-content').append(pager);
 
-  var thumbnailUrls = [];
+  var galleryElements = [];
 
-  jQuery.each(jQuery(table).find('td'), function(index, row) {
-    thumbnailUrls.push(jQuery(row).find('img').attr('src'));
+  jQuery.each(tables, function(index, table) {
+    galleryElements.push({
+      "type": "header",
+      "value": jQuery(table).prev().text()
+    });
+    jQuery.each(jQuery(table).find('td'), function(index, row) {
+      galleryElements.push({
+        "type": "thumbnailUrl",
+        "value": jQuery(row).find('img').attr('src')
+      });
+    });
   });
-  generateInitialMarkup(thumbnailUrls);
+
+  console.log(galleryElements);
+  generateInitialMarkup(galleryElements);
 
 
-  function generateInitialMarkup(thumbnailUrls) {
+  function generateInitialMarkup(galleryElements) {
     var allImageElements = [];
-    jQuery.each(thumbnailUrls, function(index, thumbnailUrl) {
-      if (thumbnailUrl) {
-        var fullSizeImageUrl = thumbnailUrl.replace('/sites/gallery.local/files/styles/node_gallery_thumbnail/public/node_gallery', '/sites/gallery.local/files/node_gallery');
-        var element = `<a class='gallery-thumbnail' data-fancybox='gallery' href='${fullSizeImageUrl}' data-thumbmail-image='${thumbnailUrl}'>
-        <div class='gallery-thumbnail-inner' style='background-image: none; display: none;'></div>
-        </a>`;
-        jQuery('.image-gallery').append(element);
+    jQuery.each(galleryElements, function(index, galleryElement) {
+      var element;
+      if (galleryElement.type === "header") {
+        element = `<h3 style='display: none'>${galleryElement.value}</h3>`
+      } else if (galleryElement.type === "thumbnailUrl") {
+        if (galleryElement.value) {
+          var fullSizeImageUrl = galleryElement.value.replace('/sites/gallery.local/files/styles/node_gallery_thumbnail/public/node_gallery', '/sites/gallery.local/files/node_gallery');
+            element = `<a class='gallery-thumbnail' data-fancybox='gallery' href='${fullSizeImageUrl}' data-thumbmail-image='${galleryElement.value}'>
+          <div class='gallery-thumbnail-inner' style='background-image: none; display: none;'></div>
+          </a>`;
+
+        }
       }
+      jQuery('.image-gallery').append(element);
+      jQuery('.image-gallery').find('h3:first').css('display', 'block');
     });
     loadImageBatch('from generateInitialMarkup');
   }
@@ -55,11 +74,16 @@ jQuery(document).ready(function() {
     var firstIndex = batchSize*iteration-batchSize;
     var thisBatch = galleryItems.slice(firstIndex, lastIndex);
     var imagesLoaded = 0;
+    console.log(thisBatch);
     jQuery.each(thisBatch, function(index, element) {
       var thumbnailElement = jQuery(this);
       var thumbnailPath = jQuery(this).attr("data-thumbmail-image");
       thumbnailElement.children('.gallery-thumbnail-inner').css("background-image", `url(${thumbnailPath})`).css("display", "block");
       setGalleryImageHeight(thumbnailElement);
+      if (jQuery(this).next('h3').length > 0) {
+        var bottomMargin = jQuery(this).next('h3').css('marginBottom');
+        jQuery(this).next('h3').attr('style', `display:block; margin-top: ${bottomMargin}; float: left; width: 100%`);
+      }
       // Create a fake image element in memory with src set to the thumbnail path, as this gives us a way to know when the image has finished loading.
       jQuery('<img/>').attr('src', thumbnailPath).on('load', function() {
         // Remove the fake image element from memory as soon as it has finished downloading, so as not to waste memory.
