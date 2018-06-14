13ac8533cd22f6c74e7cc54c18ea2a35e6cd19a9
Ubuntu <ubuntu@ip-172-31-0-102.us-west-2.compute.internal>
2018-06-14 10:07:46 +0000

    To infinity... and beyond

--- sites/all/themes/afrikaburn/template.php
+++ sites/all/themes/afrikaburn/template.php
@@ -2,6 +2,6 @@
 
 function afrikaburn_preprocess_page(&$variables) {
   if (isset($variables['node']->type)) {
-   $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
+    $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
   }
 } 
