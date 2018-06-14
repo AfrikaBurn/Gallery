7de596ad81697f0c7d3e0b9b592e68733cd50037
Ubuntu <ubuntu@ip-172-31-0-102.us-west-2.compute.internal>
2018-06-14 09:14:37 +0000

    Added gallery js to gallery node

--- sites/all/themes/afrikaburn/template.php
+++ sites/all/themes/afrikaburn/template.php
@@ -1,3 +1,7 @@
 <?php
 
-
+function afrikaburn_preprocess_page(&$variables) {
+  if (isset($variables['node']->type)) {
+   $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
+  }
+} 
